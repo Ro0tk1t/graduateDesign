0acd9baa310fea25dbaab2c47c9158a474c08eed
Ro0tk1t <1576235794@qq.com>
2018-04-22 22:38:58 +0800

    update main url path TestCase

--- test/test_basic.py
+++ test/test_basic.py
@@ -9,12 +9,14 @@ class BasicTest(unittest.TestCase):
     def setUp(self):
         self.app = create_app('test')
         self.app_context = self.app.app_context()
+        self.client = self.app.test_client()
         self.app_context.push()
 
     def tearDown(self):
         db.session.remove()
         self.app_context.pop()
 
+class Test_Config(BasicTest):
     def test_app_exists(self):
         self.assertTrue(current_app)
 
--- test/test_urls.py
+++ test/test_urls.py
@@ -2,10 +2,57 @@
 # coding=utf-8
 
 import unittest
-#import
+from flask import url_for
+from test_basic import BasicTest
 
-class Test_Root_Path(unittest.TestCase):
+class Test_URL_Path(BasicTest):
     def setUp(self):
-        pass
+        super(Test_URL_Path, self).setUp()
     def tearDown(self):
         pass
+
+    def test1(self):
+        print('*******************************')
+        #print(dir(self.app))
+
+    def test_root(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsNotNone(res.data)
+
+    def test_login_page(self):
+        res = self.client.get('login')
+        text = res.data.decode()
+        self.assertIn('username', text)
+        self.assertIn('password', text)
+
+    def test_login(self):
+        data = {'username': 'test',
+                'password': 'test'}
+        res = self.client.post('login', data=data, follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+
+    def test_logout(self):
+        res = self.client.get('logout')
+        text = res.data.decode()
+        self.assertEqual(res.status_code, 302)
+        self.assertIn('<a href="login">login</a>', text)
+
+    def test_drug(self):
+        res = self.client.get('drug/', follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('403', res.data.decode())
+        self.assertIn('Permission Deny!', res.data.decode())
+
+    def test_admin(self):
+        res = self.client.get('admin/')
+        data = res.data.decode()
+        self.assertNotIn('User', data)
+        self.assertNotIn('Wallt', data)
+        self.assertNotIn('Orders', data)
+
+    def test_user_home(self):
+        res = self.client.get('home/test')
+        data = res.data.decode()
+        self.assertIn('403', res.data.decode())
+        self.assertIn('Permission Deny!', res.data.decode())
